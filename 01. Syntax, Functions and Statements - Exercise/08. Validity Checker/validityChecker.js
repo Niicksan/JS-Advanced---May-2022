e1728c8a3c39529ad2957dd00f74ea4676249199
NIkolay Koev <nikslotar@abv.bg>
2022-05-19 23:28:18 +0300

    Syntax, Functions and Statements - Exercise

--- 01. Syntax, Functions and Statements - Exercise/08. Validity Checker/validityChecker.js
+++ 01. Syntax, Functions and Statements - Exercise/08. Validity Checker/validityChecker.js
@@ -1,6 +1,26 @@
-function validityChecker() {
+function validityChecker(x1, y1, x2, y2) {
+    let formula1 = Math.sqrt(Math.pow(0 - x1, 2) + Math.pow(0 - y1, 2));
+    let formula2 = Math.sqrt(Math.pow(x2 - 0, 2) + Math.pow(y2 - 0, 2));
+    let formula3 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
+    if (formula1 % 1 == 0) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
+
+    if (formula2 % 1 == 0) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+
+    if (formula3 % 1 == 0) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
 }
 
-validityChecker();
-validityChecker();
+validityChecker(3, 0, 0, 4);
+validityChecker(2, 1, 1, 1);
