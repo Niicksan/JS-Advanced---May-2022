678df3645c2c36b430c7814778eb9d274f4115fa
NIkolay Koev <nikslotar@abv.bg>
2022-06-02 21:53:38 +0300

    DOM Introduction - Exercise

--- 04. DOM Introduction - Exercise/08. Generate Report/report.js
+++ 04. DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,34 @@
 function generateReport() {
-    //TODO
+    let checkBoxes = document.querySelectorAll("thead tr th input");
+    let colPosition = [];
+
+    for (let i = 0; i < checkBoxes.length; i++) {
+        if (checkBoxes[i].checked) {
+            colPosition.push(i);
+        }
+    }
+
+    let finalResult = [];
+    let tableData = document.querySelectorAll("tbody tr");
+
+    for (let i = 0; i < tableData.length; i++) {
+        let currRow = tableData[i].children;
+        let currItems = {};
+
+        for (let j = 0; j < currRow.length; j++) {
+            if (colPosition.includes(j)) {
+                let label = checkBoxes[j].name;
+                let value = currRow[j].textContent;
+
+                currItems[label] = value;
+            }
+        }
+
+        finalResult.push(currItems);
+    }
+
+    if (colPosition.length > 0) {
+        let jsonFinal = JSON.stringify(finalResult);
+        document.getElementById('output').value = jsonFinal;
+    }
 }
--- 04. DOM Introduction - Exercise/09. Number Convertor/solution.js
+++ 04. DOM Introduction - Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,19 @@
 function solve() {
+    document.querySelector('#selectMenuTo option').textContent = 'Binary';
+    document.querySelector('#selectMenuTo option').value = 'binary';
+    const selectTo = document.getElementById('selectMenuTo');
+    selectTo.innerHTML += '<option selected value="hexadecimal">Hexadecimal</option>';
 
-    //TODO...
+    document.querySelector('#container button').addEventListener('click', convert);
+
+    function convert() {
+        const input = Number(document.getElementById('input').value);
+        convertedNumber = '';
+
+        selectTo.value === 'binary'
+            ? convertedNumber = input.toString(2)
+            : convertedNumber = input.toString(16).toUpperCase();
+
+        document.getElementById('result').value = convertedNumber;
+    }
 }
